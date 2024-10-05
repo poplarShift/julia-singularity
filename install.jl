43697296f0f6691d88fc5a35be0f4c62ca1524e4
doppler <ara@akvaplan.niva.no>
2024-10-05 14:36:23 +0200

    more packages needed

--- install.jl
+++ install.jl
@@ -1,11 +1,14 @@
 #!/usr/bin/env julia
 using Pkg
 Pkg.add([
+    Pkg.PackageSpec(name="ComponentArrays", version="0.14.1"),
     Pkg.PackageSpec(name="CSV", version="0.10.11"),
     Pkg.PackageSpec(name="DataFrames", version="1.6.1"),
+    Pkg.PackageSpec(name="DataFramesMeta", version="0.14.1"),
     Pkg.PackageSpec(name="DiffEqParamEstim", version="2.0.1"),
     Pkg.PackageSpec(name="DiffEqSensitivity", version="6.79.0"),
     Pkg.PackageSpec(name="DifferentialEquations", version="7.7.0"),
+    Pkg.PackageSpec(name="Interpolations", version="0.14.7"),
     Pkg.PackageSpec(name="ForwardDiff", version="0.10.36"),
     Pkg.PackageSpec(name="ModelingToolkit", version="8.50.0"),
     Pkg.PackageSpec(name="Optimization", version="3.14.0"),
@@ -16,5 +19,6 @@ Pkg.add([
     Pkg.PackageSpec(name="Plots", version="1.39.0"),
     Pkg.PackageSpec(name="RecursiveArrayTools", version="2.38.10"),
     Pkg.PackageSpec(name="SciMLSensitivity", version="7.29.0"),
+    Pkg.PackageSpec(name="StructuralIdentifiability", version="0.4.10"),
     Pkg.PackageSpec(name="Zygote", version="0.6.64"),
 ])
