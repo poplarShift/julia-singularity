f007775a4fbd0daaccc827abcb654ad5eaee8a9b
doppler <ara@akvaplan.niva.no>
2024-10-30 09:45:09 +0100

    job array runs over replicas now instead of lakes

--- batchsubmit.sh
+++ batchsubmit.sh
@@ -1,18 +1,22 @@
 #!/bin/env bash
 
-#SBATCH --job-name=whatevs
+#SBATCH --job-name=something-something-identifying
 #SBATCH --account=nn9849k
 #SBATCH --time=1-0:0:0
 #SBATCH --ntasks=1 --cpus-per-task=2 --mem-per-cpu=8GB
-#SBATCH --array=1-10
+#SBATCH --array=1-20
+
+### USAGE, e.g.:
+###
+### for lake in Ieŝjávri lake2 lake3; do
+###     sbatch --job-name=$lake ./batchsubmit.sh $lake
+### done
+###
+###
 
 set -o errexit # exit on errors
 set -o nounset # treat unset variables as errors
 
-lakes="Ieŝjávri foobar whatevs"
+lake=$1 # name of lake passed as first argument
 
-lake=$(echo $lakes|cut -d' ' -f$SLURM_ARRAY_TASK_ID)
-
-for run in {1..20}; do
-    ./julia.sif demo.jl $lake $run
-done
+./julia.sif demo.jl $lake $SLURM_ARRAY_TASK_ID
