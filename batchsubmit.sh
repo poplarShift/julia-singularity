4ea6ae074eb1c6d1a206aae9867ca6e2b50b6ffb
doppler <ara@akvaplan.niva.no>
2024-10-30 09:27:50 +0100

    batch submit script

---
+++ batchsubmit.sh
@@ -0,0 +1,18 @@
+#!/bin/env bash
+
+#SBATCH --job-name=whatevs
+#SBATCH --account=nn9849
+#SBATCH --time=1-0:0:0
+#SBATCH --ntasks=1 --cpus-per-task=2 --mem-per-cpu=8GB
+#SBATCH --jobarray=1-10
+
+set -o errexit # exit on errors
+set -o nounset # treat unset variables as errors
+
+lakes="Ieŝjávri foobar whatevs"
+
+lake=$(echo $lakes|cut -d' ' -f$SLURM_ARRAY_TASK_ID)
+
+for run in {1..20}; do
+    ./julia.sif demo.jl $lake $run
+done
